9511f87ead1fea0838c21f2a2b135f92eebc4d1c
yangqihua <904693433@qq.com>
2018-02-06 00:22:37 +0800

    有点乱，先提交一把

---
+++ src/mixins/list.js
@@ -0,0 +1,17 @@
+import wepy from 'wepy'
+
+export default class testMixin extends wepy.mixin {
+    data = {
+        list: []
+    }
+    methods = {
+        tap () {
+        }
+    }
+
+    onShow() {
+    }
+
+    onLoad() {
+    }
+}
