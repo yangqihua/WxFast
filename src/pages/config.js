f9a8b15729f2e5513122c22e2b86d158e3b41c8d
yangqihua <904693433@qq.com>
2018-02-12 12:40:29 +0800

    完成布局demo

---
+++ src/pages/config.js
@@ -0,0 +1,27 @@
+export const components = [
+    {
+        name: 'Layout',
+        path: '/pages/example/layout',
+    },
+
+    {
+        name: 'Cell',
+        path: '/pages/example/cell',
+    },
+
+    {
+        name: 'Panel',
+        path: '/pages/example/panel',
+    },
+
+    {
+        name: 'Load-More',
+        path: '/pages/example/load-more',
+    },
+
+    {
+        name: 'Abnor 异常流',
+        path: '/pages/example/abnor',
+    },
+
+]
