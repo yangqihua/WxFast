a32a471d97050a4580a47d43601fc1950bdc50d9
yangqihua <904693433@qq.com>
2018-02-25 18:38:40 +0800

    完善登录

---
+++ src/service/config/env.js
@@ -0,0 +1,5 @@
+
+const key = '5C017EC7FF0045D1B8B00076067D3B3B'
+const projectId = 'B93900E2-26AD-4AE0-BC3F-E639C2D482A0'
+const baseUrl = 'http://139.159.243.8:8001'
+export { baseUrl, key, projectId }
