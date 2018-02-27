b759207573e47337ce5022aca4b41a8fb8053f18
yangqihua <904693433@qq.com>
2018-02-27 16:02:52 +0800

    完善http请求

--- src/widgets/common/js/http.js
+++ src/widgets/common/js/http.js
@@ -6,27 +6,18 @@ import tip from './tip'
 import md5 from './md5'
 import {BASE_URL, PROJECTID, KEY} from '../../../service/config/env'
 
-
-const TIMESTAMP = new Date().getTime()
-const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
+const TIMESTAMP = Math.round(new Date().getTime() / 1000)
 
 class HTTPUtil {
-    static  getSign(url) {
-        let sign =
-            TIMESTAMP +
-            url.toLowerCase() +
-            KEY +
-            PROJECTID +
-            '不知道是什么东西'
-        return md5(sign)
-    }
-
     static async wxRequest(url, params = {}, isLoading = true, method = 'GET', headers = {}) {
-        let sign = HTTPUtil.getSign()
+        let sign = HTTPUtil.getSign(url)
+        let token = HTTPUtil.getToken(url)
+        let userId = HTTPUtil.getUserId()
+
         headers['content-type'] = 'application/json'
         isLoading && tip.loading()
         let result = await wepy.request({
-            url: BASE_URL + '/api/' + url + '?sign=' + sign + '&timestamp=' + TIMESTAMP,
+            url: BASE_URL + '/api/' + url + '?sign=' + sign + '&token=' + token + '&userId=' + userId + '&timestamp=' + TIMESTAMP,
             data: params,
             method: method,
             header: headers,
@@ -42,7 +33,6 @@ class HTTPUtil {
                 tip.error('code:' + result['statusCode'])
             }
         }
-        console.log('result:', result)
         return result
     }
 
@@ -52,12 +42,33 @@ class HTTPUtil {
         if (json) {
             // TODO: 根据业务code去判断异常
             let result = json.data
-            if (result.hasOwnProperty('code') && result['code'] == 200) {
+            if (result.hasOwnProperty('status') && result['status'] == 1) {
                 return result
             }
             errorMessage = '业务异常'
         }
         throw errorMessage;
+    }
+
+    static getSign(url) {
+        let sign =
+            TIMESTAMP +
+            url.toLowerCase() +
+            KEY +
+            PROJECTID +
+            'localhost'
+        return md5.hex_md5(sign)
+    }
+
+    static getToken(url) {
+        let userInfo = wepy.getStorageSync('userInfo')
+        let userName = userInfo.userName && userInfo.userName.toLowerCase() || ''
+        return url.toLowerCase() + TIMESTAMP + userName
+    }
+
+    static getUserId() {
+        let userInfo = wepy.getStorageSync('userInfo')
+        return userInfo.userId || ''
     }
 
 }
