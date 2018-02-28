4e079319cf2932a78a75b26c8af1f9eac16d95d7
yangqihua <904693433@qq.com>
2018-02-28 18:30:43 +0800

    首页界面搞定

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -9,7 +9,7 @@ const updateUserInfo = (params) => http.put(params, 'User/Update',true);
 const sendCode = (params) => http.post(params, 'common/sendmobileVerifiyCode',true);
 
 const getLink = (params) => http.get(params, 'diy/getlink',false);
-const getGoodList = (params) => http.get(params, 'product/list',true);
+const getGoodList = (params) => http.get(params, 'product/list',false);
 
 export {
     getLink,
