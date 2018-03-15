5056810ccc01f3bdc26bd4533150d22b33c79a0f
yangqihua <904693433@qq.com>
2018-03-15 18:59:42 +0800

    优化确认下单页面

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -3,31 +3,32 @@
  */
 
 import http from '../../widgets/common/js/http'
-const login = (params) => http.get(params, 'ApiStore/Login',false);
-const register = (params) => http.post(params, 'User/Reg',true);
-const updateUserInfo = (params) => http.put(params, 'User/Update',true);
-const sendCode = (params) => http.post(params, 'common/sendmobileVerifiyCode',true);
+const login = (params) => http.get(params, 'ApiStore/Login', false);
+const register = (params) => http.post(params, 'User/Reg', true);
+const updateUserInfo = (params) => http.put(params, 'User/Update', true);
+const sendCode = (params) => http.post(params, 'common/sendmobileVerifiyCode', true);
 
-const getLink = (params) => http.get(params, 'diy/getlink',false);
-const getGoodList = (params) => http.get(params, 'product/list',false);
-const getCategoryList = (params) => http.get(params, 'product/class',false);
-const getListByLogin = (params) => http.get(params, 'diy/GetListByLogin',false);
+const getLink = (params) => http.get(params, 'diy/getlink', false);
+const getGoodList = (params) => http.get(params, 'product/list', false);
+const getCategoryList = (params) => http.get(params, 'product/class', false);
+const getListByLogin = (params) => http.get(params, 'diy/GetListByLogin', false);
 
-const getGoodsDetails = (params) => http.get(params, 'product/show',true);
+const getGoodsDetails = (params) => http.get(params, 'product/show', true);
 
-const addCart = (params) => http.post(params, 'cart/addcart',true);
-const getCart = (params) => http.get(params, 'cart/getcart',false);
-const removeCart = (params) => http.delete(params, 'cart/removecart',true);
-const updateCart = (params) => http.put(params, 'cart/updatecart',true);
+const addCart = (params) => http.post(params, 'cart/addcart', true);
+const getCart = (params) => http.get(params, 'cart/getcart', false);
+const removeCart = (params) => http.delete(params, 'cart/removecart', true);
+const updateCart = (params) => http.put(params, 'cart/updatecart', true);
 
-const addAction = (params) => http.post(params, 'User/AddAction',true);
-const removeAction = (params) => http.delete(params, 'User/RemoveAction',true);
+const addAction = (params) => http.post(params, 'User/AddAction', true);
+const removeAction = (params) => http.delete(params, 'User/RemoveAction', true);
 
-const addOrder = (params) => http.post(params, 'Order/Buy',true);
-const getOrder = (params) => http.get(params, 'Order/Index',true);
+const addOrder = (params) => http.post(params, 'Order/Buy', true);
+const getOrder = (params) => http.get(params, 'Order/Index', true);
 
-const buyInfo = (params) => http.post(params, 'Order/BuyInfo',true);
-const getUserAddress = (params) => http.post(params, 'UserAddress/Single',true);
+const buyInfo = (params) => http.post(params, 'Order/BuyInfo', true);
+const getPrice = (params) => http.post(params, 'order/getPrice', false);
+const getUserAddress = (params) => http.post(params, 'UserAddress/Single', false);
 
 export {
     getLink,
@@ -48,5 +49,6 @@ export {
     addOrder,
     getOrder,
     buyInfo,
-    getUserAddress
+    getUserAddress,
+    getPrice
 }
